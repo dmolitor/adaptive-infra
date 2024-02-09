06a2ba4bfebd73b68af533f4bc8f41c42d6c430d
dmolitor <molitdj97@gmail.com>
2024-02-09 15:30:11 -0500

    Standardize environment variable names

--- api/connect.py
+++ api/connect.py
@@ -11,9 +11,9 @@ initialized in `tables.py`
 env_vars = os.environ
 
 ADAPTIVE_TESTING = env_vars.get("ADAPTIVE_TESTING")
-DB_PASS = env_vars["POSTGRES_PASS"]
+DB_PASS = env_vars["POSTGRES_PASSWORD"]
 DB_PORT = int(env_vars["POSTGRES_HOST_PORT"])
-DB_USER = env_vars["POSTGRES_USER_NAME"]
+DB_USER = env_vars["POSTGRES_USER"]
 
 # Creat the db URL and connect via SQLAlchemy/SQLModel
 if ADAPTIVE_TESTING is not None and ADAPTIVE_TESTING:
