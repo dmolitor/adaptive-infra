0779de8db1c890c19a2e96de1a20213318a0c9a1
Daniel Molitor <molitdj97@gmail.com>
2024-02-24 16:24:46 -0500

    Resolve merge conflicts in `ui_survey.py` :boom:

--- ui/ui_survey.py
+++ ui/ui_survey.py
@@ -12,22 +12,6 @@ bit of data prep via the API to ensure the database is storing essential data.
 # Set file paths relative to ui_cartoons.py instead of being absolute
 cur_dir = Path(__file__).resolve().parent
 
-# Get a list of all the potential options (cartoons) the user will pick from
-items = [file for file in os.listdir(str(cur_dir / "img")) if file != ".DS_Store"]
-
-# Add choices to the database if choices don't exist in there yet
-if not get_choices():
-    choices_json = {
-        "choices": items,
-        "distribution": "beta",
-        "params": {"a": 1, "b": 1, "size": 1},
-    }
-    add_choices(choices_json)
-
-# Grab the two cartoons with the top randomly generated parameter values.
-# All the cartoon parameters start with an uninformative prior Beta(1, 1)
-selected = list(top_param(n=2).keys())
-
 # UI for the survey questions
 survey_ui = ui.nav_panel(
     ui.head_content(ui.include_css("table-styles.css")),
