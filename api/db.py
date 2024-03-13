e3df9c2bf494fce70dff48c2c3f5cc8f44571dde
Daniel Molitor <molitdj97@gmail.com>
2024-03-13 16:25:29 -0400

    Functions for operationalizing the API :telephone:

--- api/db.py
+++ api/db.py
@@ -1,6 +1,7 @@
+from randomize import draw_arms
 from sqlalchemy import Engine
 from sqlmodel import Session, SQLModel, select
-from tables import Bandit, Metadata, Parameters, Response 
+from tables import Bandit, Batch, Metadata, Parameters, Pi, Response 
 from typing import List
 
 """
@@ -12,6 +13,15 @@ def create_tables(engine: Engine):
     """Creates the tables specified in `tables.py` in the Postgres db"""
     SQLModel.metadata.create_all(engine)
 
+def decrement_batch(batch_id: int, active: bool, engine: Engine):
+    """Decrement the `remaining` parameter of a given batch"""
+    with Session(engine) as session:
+        batch_obj = session.exec(select(Batch).where(Batch.id == batch_id)).one()
+        batch_obj.remaining = batch_obj.remaining - 1
+        batch_obj.active = active
+        session.add(batch_obj)
+        session.commit()
+
 def generate_bandit(labels: List[str], engine: None | Engine):
     """
     Initialize our Bandit table
@@ -22,6 +32,39 @@ def generate_bandit(labels: List[str], engine: None | Engine):
             session.add(arm)
             session.commit()
         session.commit()
+
+def generate_batch(
+    labels: List[str],
+    remaining: int,
+    active: bool,
+    pi: dict,
+    params: dict,
+    engine: None | Engine
+):
+    """
+    Initialize our Bandit pi (% of sims each arm is max discriminatory) table
+    as well as the batch table.
+
+    `params` should be a dictionary as described above in `generate_parameters`
+
+    E.g.
+    labels = ["arm1", "arm2", "arm3", "arm4"]
+    """
+    # Generate new row in the `Batch` table
+    with Session(engine) as session:
+        batch_obj = Batch(remaining=remaining, active=active)
+        session.add(batch_obj)
+        session.commit()
+        batch_id = batch_obj.id
+    # Generate new values in the `Parameters` table
+    generate_parameters(
+        labels=labels,
+        batch_id=batch_id,
+        params=params,
+        engine=engine
+    )
+    # Generate new values in the `Pi` table
+    generate_pi(labels=labels, batch_id=batch_id, pi=pi, engine=engine)
 
 def generate_bandit_metadata(
     labels: List[str],
@@ -59,32 +102,26 @@ def generate_bandit_metadata(
         # TODO: Is this last session.commit really necessary???
         session.commit()
 
-def get_bandit(engine):
-    """Retrieve a list of all bandit arms"""
-    with Session(engine) as session:
-        bandit = session.exec(select(Bandit)).all()
-        out = list()
-        for arm in bandit:
-            out.append({
-                "arm": arm,
-                "parameters": arm.parameters,
-                "metadata": arm.meta
-            })
-    return out
-
-def get_metadata(engine):
-    """Retrieve a list of all metadata items"""
-    with Session(engine) as session:
-        metadata = session.exec(select(Metadata)).all()
-    return metadata
-
 def generate_parameters(
     labels: List[str],
+    batch_id: int,
     params: dict,
     engine: None | Engine
 ):
     """
     Initialize our Bandit parameters table
+
+    `params` is a dictionary with keys equal to `labels` and the values
+    being a dictionary containing "alpha" and "beta" values parameterizing
+    the beta distribution in each arm. E.g.
+    ```
+    params: {
+        "arm1": {"alpha": 1, "beta": 1},
+        "arm2": {"alpha": 1, "beta": 1},
+        "arm3": {"alpha": 1, "beta": 1},
+        "arm4": {"alpha": 1, "beta": 1}
+    }
+    ```
     """
     with Session(engine) as session:
         for label in labels:
@@ -96,23 +133,55 @@ def generate_parameters(
                 )
                 .one()
             )
+            arm_params = params[label]
             param_obj = Parameters(
                 arm_id=arm.id,
-                alpha = params["alpha"],
-                beta = params["beta"]
+                batch_id=batch_id,
+                alpha = arm_params["alpha"],
+                beta = arm_params["beta"]
             )
             session.add(param_obj)
             session.commit()
         session.commit()
 
-def get_parameters(engine):
-    """Retrieve a list of all bandit arm parameters"""
+def generate_pi(
+    labels: List[str],
+    batch_id: int,
+    pi: dict,
+    engine: None | Engine
+):
+    """
+    Initialize our Bandit Pi table
+
+    `pi` should be a dictionary where the keys are the `labels`, and the values
+    are floats indicating the % of realizations drawn from the posterior
+    distribution of each arm that the corresponding arm is the max/min
+    discriminatory context.
+
+    E.g.
+    pi = {"arm1": 0.25, "arm2": 0.5, "arm3": 0.75, "arm4": 1}
+    """
+    # Generate new values in the `Pi` table
     with Session(engine) as session:
-        params = session.exec(select(Parameters)).all()
-    return params
+        for label in labels:
+            # Get the corresponding Bandit arm
+            arm: Bandit = (
+                session
+                .exec(
+                    select(Bandit).where(Bandit.label == label)
+                )
+                .one()
+            )
+            # Get corresponding pi value
+            arm_pi = pi[label]
+            pi_obj = Pi(batch_id=batch_id, arm_id=arm.id, pi=arm_pi)
+            session.add(pi_obj)
+            session.commit()
 
 def generate_response(
     consent: bool,
+    arm_id: int,
+    batch_id: int,
     prolific_id: str | None,
     in_usa: bool | None,
     commitment: str | None,
@@ -131,6 +200,8 @@ def generate_response(
     with Session(engine) as session:
         response_obj = Response(
             consent=consent,
+            arm_id=arm_id,
+            batch_id=batch_id,
             prolific_id=prolific_id,
             in_usa=in_usa,
             commitment=commitment,
@@ -148,48 +219,132 @@ def generate_response(
         session.commit()
     return True
 
+def get_bandit(engine) -> List[Bandit]:
+    """Retrieve a list of all bandit arms"""
+    with Session(engine) as session:
+        bandit = session.exec(select(Bandit)).all()
+        out = list()
+        for arm in bandit:
+            out.append({
+                "arm": arm,
+                "parameters": arm.parameters,
+                "metadata": arm.meta,
+                "pi": arm.pi,
+                "responses": arm.responses
+            })
+    return out
+
+def get_batch(batch_id: int, engine: Engine):
+    """Retrieve a specific Batch"""
+    with Session(engine) as session:
+        out = []
+        batch = session.exec(select(Batch).where(Batch.id == batch_id)).one()
+    return batch
+
+def get_batches(engine):
+    """Retrieve a list of all batch values"""
+    with Session(engine) as session:
+        out = []
+        batch = session.exec(select(Batch)).all()
+        for b in batch:
+            out.append({"batch": b, "parameters": b.parameters, "pi": b.pi})
+    return out
+
+def get_current_batch(engine: Engine):
+    """Get the batch id for the current batch"""
+    with Session(engine) as session:
+        batch = (
+            session.exec(
+                select(Batch)
+                .where(Batch.remaining > 0)
+                .where(Batch.active == True)
+            )
+            .one()
+        )
+    return batch
+
+def get_metadata(engine):
+    """Retrieve a list of all metadata items"""
+    with Session(engine) as session:
+        metadata = session.exec(select(Metadata)).all()
+    return metadata
+
+def get_parameters(engine: Engine):
+    """Retrieve a list of all bandit arm parameters"""
+    with Session(engine) as session:
+        params = session.exec(select(Parameters)).all()
+        out = []
+        for param in params:
+            out.append({"parameters": param, "batch": param.batch})
+    return out
+
+def get_pi(engine):
+    """Retrieve a list of all pi values"""
+    with Session(engine) as session:
+        pi = session.exec(select(Pi)).all()
+        out = []
+        for p in pi:
+            out.append({"pi": p, "batch": p.batch})
+    return out
+
 def get_responses(engine: None | Engine):
     """Retrieve a list of all responses"""
     with Session(engine) as session:
         responses = session.exec(select(Response)).all()
     return responses
 
-def update_params(arm_id: int, params: dict, engine: Engine):
-    """
-    Update the Beta-Bernoulli distribution parameters for a specific bandit arm
-    """
-    with Session(engine) as session:
-        param_obj = Parameters(
-            arm_id=arm_id,
-            alpha=params["alpha"],
-            beta=params["beta"]
-        )
-        session.add(param_obj)
-        session.commit()
-    return True
-
-def increment(
-    arm_id: int,
-    engine: Engine,
-    alpha: bool = False,
-    beta: bool = False
+def increment_batch(
+    batch_id: int,
+    remaining: int,
+    active: bool,
+    engine: None | Engine
 ):
-    """
-    Increment the Beta-Bernoulli distribution params for a specific bandit arm
-    """
-    with Session(engine) as session:
-        params = (
-            session
-            .exec(
-                select(Parameters).where(Parameters.arm_id == arm_id)
+    bandit = get_bandit(engine=engine)
+    # For each arm, collect successes and failures
+    labels = []
+    params = {}
+    for arm in bandit:
+        arm = arm["arm"]
+        arm_label = arm.label
+        # Construct the labels for each arm
+        labels.append(arm_label)
+        # Construct updated alpha and beta parameters for each arm
+        with Session(engine) as session:
+            arm_params = (
+                session.exec(
+                    select(Parameters)
+                    .where(Parameters.arm_id == arm.id)
+                    .where(Parameters.batch_id == batch_id)
+                )
+                .one()
             )
-            .all()
-        )
-        last_param = max(params, key=lambda x: x.id)
-        new_alpha = last_param.alpha
-        new_beta = last_param.beta
-        if alpha:
-            new_alpha += 1
-        if beta:
-            new_beta += 1
-        update_params(arm_id, {"alpha": new_alpha, "beta": new_beta}, engine)
+            arm_batch_responses = (
+                session.exec(
+                    select(Response)
+                    .where(Response.arm_id == arm.id)
+                    .where(Response.batch_id == batch_id)
+                )
+                .all()
+            )
+            successes = 0
+            failures = 0
+            for response in arm_batch_responses:
+                if response.discriminated is True:
+                    successes += 1
+                elif response.discriminated is False:
+                    failures += 1
+            params[arm_label] = {
+                "alpha": arm_params.alpha + successes,
+                "beta": arm_params.beta + failures
+            }
+    # Construct updated Pi value for each arm
+    pi = draw_arms(params)
+    # Now update the `Batch`, `Parameters` and `Pi` tables
+    generate_batch(
+        labels=labels,
+        remaining=remaining,
+        active=active,
+        pi=pi,
+        params=params,
+        engine=engine
+    )
