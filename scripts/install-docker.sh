125ae8857c2664ccdbd9e96ffc794b7912f4b7fd
Daniel Molitor <molitdj97@gmail.com>
2024-04-07 21:21:01 -0400

    Setup Packer :package:
    Packer will take care of automatically building the AMI we want to launch our adaptive server on!
    - `aws-docker.pkr.hcl` defines the Packer build steps
    - `scripts/install-docker.sh` specifies all the installation steps that Packer will follow before creating the AMI.

---
+++ scripts/install-docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -x
+
+sudo apt-get install -y gnome-terminal
+sudo apt-get update
+# TODO: Follow the instructions here to complete this: https://docs.docker.com/desktop/install/ubuntu/
